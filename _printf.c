2571d396cbc3f90b1418c22378534fe8b9385276
REGRAGUII <youssef98reg@gmail.com>
2023-06-27 19:38:12 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -24,9 +24,21 @@ int _printf(const char *format, ...)
 			putchr(va_arg(args, int));
 			h++;
 		}
+		else if (format[h + 1] == 's')
+		{
+		int r_val = put_s(va_arg(args, char *));
+
+			r_value += (r_val - 1);
+			h++;
+		}
 		else if (format[h + 1] == "%")
 		{
 			putchr('%');
+			h++;
+		}
+		else if ((format[h + 1] == 'd') || (format[h + 1] == 'i'))
+		{
+			get_int(va_arg(args, int));
 			h++;
 		}
 		r_value += 1;
