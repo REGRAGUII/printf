45144f14c231a78147bec315c85b9aa38c18ef12
REGRAGUII <youssef98reg@gmail.com>
2023-06-30 19:52:12 -0700

    up

--- _printf.c
+++ _printf.c
@@ -1,74 +1,89 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
+
+void clean_up(va_list data, buff_t *cout);
+int read_f_printf(const char *format, va_list data, buff_t *cout);
+int _printf(const char *format, ...);
 
 /**
- * _printf - prints a formatted string to stdout, similar to printf.
- * @format: the format of the string to be printed.
+ * clean_up - this function that performance cleanup operations for _printf.
+ * @data: arguments provided to _printf.
+ * @cout: buff_t struct.
+ */
+void clean_up(va_list data, buff_t *cout)
+{
+	va_end(data);
+	write(1, cout->start, cout->len);
+	free_buffer(cout);
+}
+
+/**
+ * read_f_printf - this function that Reads through the format
+ * of  string for _printf.
+ * @format: Character string to print
+ * @cout: buff_t struct containing a buffer.
+ * @data: arguments to be printed.
+ * int - ..
+ * Return: The number of characters stored to @cout.
+ */
+int read_f_printf(const char *format, va_list data, buff_t *cout)
+{
+	int i, width, prec, ret_n = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buff_t *, unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flag(format + i + 1, &tmp);
+			width = handle_width(data, format + i + tmp + 1, &tmp);
+			prec = handle_prec(data, format + i + tmp + 1,
+					&tmp);
+			len = handle_len(format + i + tmp + 1, &tmp);
+
+			f = handle_specif(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret_n += f(data, cout, flags, width, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret_n = -1;
+				break;
+			}
+		}
+		ret_n += _memcpy(cout, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	clean_up(data, cout);
+	return (ret_n);
+}
+
+/**
+ * _printf - this function that prints a formatted string.
+ * @format: Characters of string
  *
- * This function prints a formatted string to the stdout stream. It
- * accepts a format string as its first argument and any additional arguments
- * will be used to replace format specifiers in the format string.
- *
- * Return: the number of characters printed to the stdout stream.
+ * Return: The number of char printed.
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed = 0;
+	buff_t *cout;
+	va_list data;
+	int ret_n;
 
-	if (!format)
-		return -1;
+	if (format == NULL)
+		return (-1);
+	cout = _buffer();
+	if (cout == NULL)
+		return (-1);
 
-	va_start(args, format);
+	va_start(data, format);
+	ret_n = read_f_printf(format, data, cout);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				putchar(c);
-				printed++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				if (s)
-				{
-					while (*s)
-					{
-						putchar(*s);
-						printed++;
-						s++;
-					}
-				}
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-				printf("%d", num);
-				printed++;
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				printed++;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			printed++;
-		}
-
-		format++;
-	}
-
-	va_end(args);
-
-	return printed;
+	return (ret_n);
 }
